cde0c05ce1ec3d73422189635c7ee4d23200458d
JohnVX <wswbpf@163.com>
2017-09-02 15:54:30 +0800

    ok

---
+++ for.c
@@ -0,0 +1,15 @@
+/* for.c A sample C for program */
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+	int j;
+
+	for(i = 0; i < 10; i++)
+	{
+		j = i * 5;
+		printf("The answer is %d\n", j);
+	}
+	return 0;
+}
---
+++ ifthen.s
@@ -0,0 +1,43 @@
+	.file	"ifthen.c"
+	.section	.rodata
+.LC0:
+	.string	"The higher value is %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$100, -8(%rbp)
+	movl	$25, -4(%rbp)
+	movl	-8(%rbp), %eax
+	cmpl	-4(%rbp), %eax
+	jle	.L2
+	movl	-8(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	jmp	.L3
+.L2:
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+.L3:
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
